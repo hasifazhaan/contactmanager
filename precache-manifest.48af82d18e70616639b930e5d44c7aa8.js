5cced93b89461ee9ec3cfe2c5adea96060920267
hasifazhaan <hasif45azhaan@gmail.com>
2020-09-23 20:34:41 +0530

    Updates

---
+++ precache-manifest.48af82d18e70616639b930e5d44c7aa8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "296266a7a1c9caf45697224f79a9a3b9",
+    "url": "/contactmanager/index.html"
+  },
+  {
+    "revision": "877b28ab4645e70f05cb",
+    "url": "/contactmanager/static/css/main.8a211c88.chunk.css"
+  },
+  {
+    "revision": "1f8143cc8cc9c8b2a926",
+    "url": "/contactmanager/static/js/2.e1b81f81.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/contactmanager/static/js/2.e1b81f81.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "877b28ab4645e70f05cb",
+    "url": "/contactmanager/static/js/main.f0ebe355.chunk.js"
+  },
+  {
+    "revision": "71d5008f592274bfcb99",
+    "url": "/contactmanager/static/js/runtime-main.b7fc99b5.js"
+  }
+]);
